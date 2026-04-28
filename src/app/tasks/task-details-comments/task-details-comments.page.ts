d2af8dc49b27f4401a0b4c7df3afc21e125c38d7
JP27 <joanpomaresherrero@gmail.com>
2026-04-28 17:16:37 +0200

    pagina de comentarios

--- src/app/tasks/task-details-comments/task-details-comments.page.ts
+++ src/app/tasks/task-details-comments/task-details-comments.page.ts
@@ -1,35 +1,137 @@
-import { Component, inject, input, numberAttribute, OnInit, signal } from '@angular/core';
+import { Component, inject, input, linkedSignal, numberAttribute, OnInit, Signal, signal, WritableSignal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, Platform } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, Platform, IonList, IonListHeader, IonItem, IonCard, IonCardHeader, IonAvatar, IonImg, IonLabel, IonCol, IonRow, IonFab, IonFabButton, IonIcon, AlertController, IonInfiniteScroll, IonInfiniteScrollContent } from '@ionic/angular/standalone';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { TaskService } from '../services/task-service';
+import { Comment, CommentResponse } from '../interfaces/task';
+import { addIcons } from 'ionicons';
+import { add, text } from 'ionicons/icons';
+import { InfiniteScrollCustomEvent, IonInfiniteScrollCustomEvent } from '@ionic/core';
 
 @Component({
   selector: 'app-task-details-comments',
   templateUrl: './task-details-comments.page.html',
   styleUrls: ['./task-details-comments.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    IonList,
+    IonListHeader,
+    IonItem,
+    IonCard,
+    IonCardHeader,
+    IonAvatar,
+    IonImg,
+    IonLabel,
+    IonCol,
+    IonRow,
+    IonFab,
+    IonFabButton,
+    IonIcon,
+    IonInfiniteScroll,
+    IonInfiniteScrollContent
+]
 })
 export class TaskDetailsCommentsPage {
 
+
   #platform = inject(Platform)
   #service = inject(TaskService)
+  #alertController = inject(AlertController)
 
   id = input.required({transform: numberAttribute});
   page = signal<number>(1)
 
+  
+
   #commentsResoure = this.#service.getCommentsResource(this.id, this.page)
+  comments = linkedSignal<CommentResponse | undefined, Comment[]>({
+    source: () => this.#commentsResoure.hasValue() ? this.#commentsResoure?.value() : {
+      comments: []
+    },
+    computation: (resp, prev) => {
+      if(!resp) return prev?.value ?? [];
+      return this.page() > 1 && prev ? prev.value.concat(resp!.comments) : resp?.comments;
+    },
+  })
+
+  public async addComment() {
+    const alert = await this.#alertController.create({
+      header: 'Nuevo comentario',
+      inputs: [{
+        name: 'comentario',
+        type: 'textarea',
+        placeholder: 'Escribe tu comentario'
+      }],
+      buttons: [{
+        text: 'Enviar',
+        role: 'ok'
+      },{
+        text: 'Cancelar',
+        role: 'cancel'
+      }]
+    });
+    await alert.present();
+    const result = await alert.onDidDismiss();
+
+    if(result.role == 'ok') {
+
+      console.log(result)
+
+      const commentario : Comment = {
+        id: 0,
+        taskId: this.id(),
+        comment: '',
+        currentPage: 0,
+        hasNextPage: false,
+        createdAt: '',
+        user: {
+          id: 0,
+          lat: 0,
+          lng: 0,
+          me: false,
+          name: '',
+          avatar: '',
+          email: ''
+        }
+      }
+
+      commentario.comment = result.data.values.comentario;
+
+      this.#service.addComment(this.id(), commentario)
+      .subscribe({
+        next: (c: Comment) => {
+          this.comments.update(cs => [c, ...cs])
+        },
+        error: (er) => console.error(er)
+      })
+    }
+  }
 
   constructor() {
       this.#platform.resume.pipe(takeUntilDestroyed()).subscribe(
         () => this.#commentsResoure.reload()
       )
+      addIcons({
+        add
+      })
    }
 
   public ionViewWillEnter() {
     this.#commentsResoure.reload()
+  }
+  onIonInfinite(event: InfiniteScrollCustomEvent) {
+    this.page.update((p) => p + 1);
+  
+    setTimeout(() => {
+      event?.target?.complete
+    }, 500);
   }
 
 }
--- src/app/tasks/task-details-info/task-details-info.page.ts
+++ src/app/tasks/task-details-info/task-details-info.page.ts
@@ -1,27 +1,34 @@
-import { Component, DestroyRef, inject, OnInit, WritableSignal } from '@angular/core';
+import { Component, DestroyRef, inject, linkedSignal, OnInit, signal, WritableSignal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { 
   IonContent,
-  IonHeader,
-  IonTitle,
-  IonToolbar, IonList,
-  IonListHeader,
-  IonItem,
-  IonInput,
-  IonTextarea,
-  IonDatetimeButton,
-  IonModal,
-  IonDatetime,
-  IonSelect,
-  IonSelectOption
-} from '@ionic/angular/standalone';
+   IonHeader,
+   IonTitle,
+   IonToolbar,
+   IonList,
+   IonListHeader,
+   IonItem,
+   IonInput,
+   IonTextarea,
+   IonDatetimeButton,
+   IonModal,
+   IonDatetime,
+   IonSelect,
+   IonSelectOption,
+   IonButton,
+   IonIcon,
+   IonImg
+  } from '@ionic/angular/standalone';
 import { TaskDetailsPage } from '../task-details/task-details.page';
 import { form, FormField, required } from "@angular/forms/signals";
 import { Task } from '../interfaces/task';
 import { minDate } from 'src/app/shared/validators/minDay';
 import { TaskService } from '../services/task-service';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { addIcons } from 'ionicons';
+import { images, link } from 'ionicons/icons';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 
 @Component({
   selector: 'app-task-details-info',
@@ -29,8 +36,8 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
   styleUrls: ['./task-details-info.page.scss'],
   standalone: true,
   imports: [
-    IonContent, 
-    IonHeader, 
+    IonContent,
+    IonHeader,
     IonTitle,
     IonToolbar,
     CommonModule,
@@ -45,26 +52,47 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
     IonModal,
     IonDatetime,
     IonSelect,
-    IonSelectOption
-  ]
+    IonSelectOption,
+    IonButton,
+    IonIcon,
+    IonImg
+]
 })
 export class TaskDetailsInfoPage {
 
-  #task = inject(TaskDetailsPage).task
+  constructor() {
+    addIcons({
+      images
+    })
+  }
+
+
+  task = inject(TaskDetailsPage).task
   #taskService = inject(TaskService)
   #destroyRef = inject(DestroyRef);
 
+  auxStatus = linkedSignal(() => this.task().status);
+  auxFilePath = linkedSignal(() => this.task().filepath);
 
-  public taskForm = form(this.#task as WritableSignal<Task>, (schema) => {
-    required(schema.title, {message: 'La tarea debe tener un titulo'}),
-    required(schema.description, {message: 'La tarea debe tener una descripcion'})
-    minDate(schema.deadLine,new Date().toISOString().split('T')[0], { message: 'la fecha no puede ser anterior a hoy' })
-  });
+  statusForm = form(this.auxStatus);
+
   
   public onStatusChange(event: Event) {
-    const result = this.#taskService.updateStatus(this.#task()!.id, this.#task()!.status)
+    const result = this.#taskService.updateStatus(this.task()!.id, this.auxStatus())
     result.pipe(takeUntilDestroyed(this.#destroyRef)).subscribe({
-      next: (t) => this.#task.update(ts => t)
+      next: (t) => this.task.update(ts => t)
     })
+  }
+
+  public async pickFromGallery() {
+    const photo = await Camera.getPhoto({
+      source: CameraSource.Photos,
+      height: 640,
+      width: 640,
+      // allowEditing: true,
+      resultType: CameraResultType.DataUrl, // Base64 (url encoded)
+    });
+
+    this.auxFilePath.set(photo.dataUrl as string);
   }
 }
